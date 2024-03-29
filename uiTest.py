30a29b0dff85eeae7d192b8b27ad5bf3ea4b5f58
brownieSea <atticroom01@gmail.com>
2024-03-29 14:46:08 +0900

    fdfdsa

--- uiTest.py
+++ uiTest.py
@@ -11,9 +11,12 @@ class MyWindow(QMainWindow, form_class):
         super().__init__()  # 부모클래스 생성자 호출
         self.setupUi(self)  # 위에서 불러온 uiTest.ui 를 연결
         self.setWindowTitle("연습 프로그램")
+        self.setWindowIcon(QIcon('google.png'))
         self.Button1.clicked.connect(self.button1Click)
         self.Button2.clicked.connect(self.button2Click)
         #(self.button2Click) 부분에서 button2Click 이 함수이므로 자동완성으로 뒤에 () 붙는데 ()는 빼줘야한다
+        self.statusBar().showMessage("copyleft")
+
 
     def button1Click(self):
         self.LabelTxt.setText("Hello World!!!")
